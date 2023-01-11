57d78eb813d26e58170fba371d08fa59cb74c133
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-01-11 18:55:01 +0330

    calendar basic.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <h1>1401</h1>
+      </header>
+      <main>
+        <section>
+          <div>
+            <strong>فروردین</strong>
+          </div>
+          <div>
+            <div>
+              <span>۱</span>
+              <span>24</span>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
