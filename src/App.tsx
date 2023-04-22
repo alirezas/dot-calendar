a574c330e67628e240256354188740ea37f51794
Alireza Sarabchi <a.sarabchi@gmail.com>
2023-04-22 18:54:42 +0330

    basic PWA config.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useReducer } from 'react'
 import Calendar from './components/Calendar'
 import Header from './components/Header'
+import ReloadPrompt from './components/ReloadPromp'
 import dayJsInstance from './config/dayJs'
 import { DayJsContext } from './context/DayJsContext'
 import { ThemeContext, themeReducer } from './context/ThemeContext'
@@ -17,11 +18,15 @@ function App(): JSX.Element {
     document.documentElement.classList.remove('dark')
   }
 
+  // replaced dyanmicaly
+  const date = '__DATE__'
+
   return (
     <>
       <ThemeContext.Provider value={{ state, dispatch }}>
         <DayJsContext.Provider value={{ dayjs: dayJsInstance }}>
           <div className="md:my-24 container px-4 mx-auto my-12">
+            <ReloadPrompt />
             <Header />
             <Calendar />
           </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Dot Calendar',
+        short_name: 'Dot Calendar',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#020617',
+        description: 'Just a Calendar.',
+        icons: [
+          {
+            src: 'images/favicon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'images/favicon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'images/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+          },
+          {
+            src: 'images/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 })
